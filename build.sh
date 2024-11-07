da74bd89f47f6a96b7963207ccd0586a65defb89
Mikhail Pogretskiy <mikhail.pogretskiy@gmail.com>
2024-11-07 22:50:57 +0100

    Add build script

--- build.sh
+++ build.sh
@@ -14,6 +14,10 @@ if command -v terser > /dev/null 2>&1; then
   echo "Uglifying..."
   echo "$ terser docs/main.js --compress passes=2,pure_getters=true -o docs/main.js"
   terser docs/main.js --compress passes=2,pure_getters=true -o docs/main.js && echo "Done"
+elif command -v uglifyjs > /dev/null 2>&1; then
+  echo "Uglifying..."
+  echo "$ uglifyjs docs/main.js --compress --mangle -o"
+  uglifyjs docs/main.js --compress --mangle -o docs/main.js
 else
-  echo "terser CLI util wasn't found, uglification skipped"
+  echo "terser/uglifyjs util wasn't found, uglification skipped"
 fi
